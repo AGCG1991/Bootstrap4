e86cce770935f27bd975ff1a12f653f3cc4f5c6e
Aitor <aitor_gustavo@hotmail.com>
2021-06-24 17:46:14 +0200

    Termino Seccion

--- CAPÍTULO 7 - FORMULARIOS/formulario.js
+++ CAPÍTULO 7 - FORMULARIOS/formulario.js
@@ -1,36 +1,29 @@
-//1º EVITAMOS QUE SE REFRESQUE AL ENVIAR
-
+//console.log("funcionando");
 $("#formulario").submit(function (event) {
-    //En cuanto detecte el submit, el cual se ejecuta al pulsar enviar, almacenamos los datos y evitamos que se refresque, una vez hecho, enviamos los datos
-    event.preventDefault(); //Almacena los datos sin refrescar el sitio web
+    event.preventDefault(); //almacena los datos sin refrescar el sitio web.
     enviar();
-})
-
+});
 function enviar() {
-    var datos = $("#formulario").serialize(); //Toma los datos "name" y los lleva a un arreglo
-    console.log(datos);
+    //console.log("ejecutado");
+    var datos = $("#formulario").serialize(); //toma los datos "name" y los lleva a un arreglo.
     $.ajax({
         type: "post",
         url: "formulario.php",
         data: datos,
-        success: function (texto) {  //Esperamos una respuesta a la que respondemos con success
-            if (texto === "exito") {
+        success: function (texto) { //Esperamos una respuesta a la que respondemos con success
+            if (texto == "exito") {
                 correcto();
-
             } else {
                 phperror(texto);
             }
         }
-
     })
 }
-
 function correcto() {
     $("#mensajeExito").removeClass("d-none"); //Elmino la clase d-none gracias a removeClass
-
+    $("#mensajeError").addClass("d-none"); //Si hubo errores antes, los eliminamos (no queremos que se acumulen en pantalla)
 }
 function phperror(texto) {
     $("#mensajeError").removeClass("d-none");
-    $("mensajeError").html(texto);
-
+    $("#mensajeError").html(texto);
 }
--- CAPÍTULO 7 - FORMULARIOS/formulario.php
+++ CAPÍTULO 7 - FORMULARIOS/formulario.php
@@ -1,48 +1,4 @@
-<?php
-$error = '';
-
-//validamos el nombre
-if(empty( $_POST["nombre"])){ //Si está vacio
-    $error = 'Ingresa un nombre </br>';
-}else {
-    $nombre = $_POST["nombre"];
-    $nombre =filter_var($nombre, FILTER_SANITIZE_STRING); //Limpiamos el texto de etiquetas 
-
-}
-//validamos el email
-if (empty($nombre = $_POST["email"];)){
-    $error .= 'Ingresa un email </br>'; //.= Concatena con el valor que ya tenía antes
-    
-}else {
-    $email = $nombre = $_POST["email"];
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){ 
-        $error .= 'Ingresa un Email verdadero </br>';
-
-    }else {
-        $email = filter_var($email,FILTER_SANITIZE_EMAIL);
-    }
-}
-
-//validamos el mensaje
-if(empty($_POST["mensaje"])){
-    $error .= 'Ingresa un mensaje </br>';
-}else {
-    $mensaje = $_POST["mensaje"];
-    $mensaje = filter_var($mensaje, FILTER_SANITIZE_STRING);
-}
-
-//CUERPO DEL MENSAJE
-$cuerpo = 'Nombre :'.$nombre.'\n'; //Concateno
-$cuerpo.= 'Email  :' .$email. '\n'; 
-$cuerpo.= 'Mensaje :'.$mensaje.'\n';
-
-//Dirección
-$enviarA = 'aitor.gustavo@gmail.com';
-$asunto = 'Nuevo mensaje de mi sitio web';
-
-//Enviar correo
-$success = mail($enviarA,$asunto,$cuerpo,'de : '.$email); //Primer parámetro correoDestino,Segundo parámetro asunto, tercero el cuerpo y cuarto, el email del cliente
-<?php
+﻿<?php
 
 $error = '';
 
@@ -65,15 +21,15 @@ if(empty($_POST["email"])){
     if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
         $error .= 'Ingresa un E-mail verdadero</br>';
     }else{
-        $email = filter_var($email,FILTER_SANITIZE_EMAIL);
+        $email = filter_var($email,FILTER_SANITIZE_EMAIL); //Limpiamos el texto de etiquetas
     }
 }
 //VALIDANDO MENSAJE
-if(empty($_POST["mensaje"])){
-    $error .= 'Ingresa un mensaje </br>';
+if(empty($_POST["mensaje"])){ //Si está vacio, devuelve error
+    $error .= 'Ingresa un mensaje </br>'; //.= Concatena con el valor que ya tenía antes
 }else{
     $mensaje = $_POST["mensaje"];
-    $mensaje = filter_var($mensaje, FILTER_SANITIZE_STRING);
+    $mensaje = filter_var($mensaje, FILTER_SANITIZE_STRING); 
     $mensaje = trim($mensaje);
     if($mensaje==''){
         $error .= 'Mensaje está vacio</br>';
@@ -94,24 +50,16 @@ $cuerpo .= $mensaje;
 $cuerpo .= "\n";
 
 //DIRECCIÓN
-$enviarA = 'tucorreo@tudominio.com'; //REEMPLAZAR CON TU CORREO ELECTRÓNICO
+$enviarA = 'aitor.gustavo@gmail.com'; //REEMPLAZAR CON TU CORREO ELECTRÓNICO
 $asunto = 'Nuevo mensaje de mi sitio web';
 
 //ENVIAR CORREO
 if($error == ''){
+    //Primer parámetro correoDestino,Segundo parámetro asunto, tercero el cuerpo y cuarto, el email del cliente
     $success = mail($enviarA,$asunto,$cuerpo,'de: '.$email);
     echo 'exito';
 }else{
     echo $error;
 }
-
-?>
-if($error == ''){
-    $success = mail($enviarA,$asunto,$cuerpo,'de : '.$email);
-    echo 'exito';
-}else {
-     echo $error;
-}
-
 
 ?>
